e7c1cdcaaece99ac2c70e5be8c4dc033135918d5
Ancient789 <83103802+Ancient789@users.noreply.github.com>
2021-06-30 20:51:28 -0700

    Add files via upload

---
+++ js/Slingshot.js
@@ -0,0 +1,26 @@
+class Slingshot {
+    constructor(body1, point2) {
+        var options = {
+            bodyA: body1,
+            pointB: point2,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options)
+        World.add(world, this.sling)
+    }
+  display() {
+      if (this.sling.bodyA) {
+      var pointA = this.sling.bodyA.position
+      var pointB = this.sling.pointB
+      strokeWeight(3)
+      line(pointA.x, pointA.y, pointB.x, pointB.y)
+      }
+  }
+  fly() {
+      this.sling.bodyA = null
+  }
+  attach(body) {
+     this.sling.bodyA = body 
+  }
+}
